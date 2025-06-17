67e9396ae738259e40c623caeda62b07b698e090
Your Name <nikhiltiwari0296@gmail.com>
2025-06-17 13:11:59 +0530

    build themswitcher app

---
+++ 10themswitcher/src/App.jsx
@@ -0,0 +1,46 @@
+import react,{ useState, useEffect } from 'react'
+
+import './App.css'
+import { ThemeProvider } from './context/theme.js'
+import Card from './componenets/Card.jsx'
+import ToggleBtn from './componenets/ToggleBtn.jsx'
+
+
+function App() {
+
+  const [themeMode, setThemeMode] = useState('light')
+
+  const lightTheme = ()=>{
+    setThemeMode("light")
+  }
+  const darkTheme = ()=>{
+    setThemeMode("dark")
+  }
+
+  
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', themeMode === 'dark');
+  }, [themeMode]);
+  return (
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ToggleBtn />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ 10themswitcher/tailwind.config.js
@@ -0,0 +1,11 @@
+ule.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  darkMode: "class", // Ensure this is set for class-based dark mode
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
